15c9541f0a79e379a7351e96af2639f32543d86e
Spandan Kumar <venkateshspandan@gmail.com>
2026-02-07 20:40:23 -0600

    Added Eureka.java with method overloading and debugging examples

---
+++ Eureka.java
@@ -0,0 +1,64 @@
+public class Eureka {
+    public static void main(String[] args) {
+        System.out.println("Eureka Java Training :(");
+        //primitive
+        int i ;
+        i = 10; //assigning
+        int j = 20;
+        System.out.println("Value of i :" + i);
+        System.out.println("Value of j :" + j);
+        //float
+
+        float f1=13.21f;
+        float f2=26.12f;
+        System.out.println("Parameterized method");
+        float v=floatAdd(f1,f2);
+        System.out.println(v);
+
+        floatAdd(f1,f2); //calling  method
+        //double
+        double d1 = 12.34;
+        double d2 = 34.12;
+        System.out.println("multi is " + doubleAddition(d1,d2));
+        double d=doubleAddition(12.01,12.01);
+        //boolean
+        boolean b1 = true;
+        b1 = false;
+        System.out.println("Value of boolean is" + b1);
+        //long
+        long l1 = 12367842;
+        long l2 = 24938341;
+        System.out.println(("Value is" + l1 + " and " + l2));
+        char c1 = 'V';
+        //non-primitive types
+        String s1 = "Eureka";
+        String s2 = new String("Tech");
+        System.out.println("concat of is " + s1 + " " + s2);
+        booleanType();
+    }
+
+    private static double doubleAddition ( double d1,double d2) {
+        return d1 * d2;
+    }
+
+    private static float floatAdd(float f1,float f2){
+        return f1+f2;
+    }
+    //class name => Eureka
+    //variable=.StringNmae
+    //method => floatAddition
+//    private static void floatAdd() { //method signature
+//        float f1 = 34.22f;
+//        float f2 = 22.34f;
+//
+//        System.out.println("Sum of 2 floats is " + (f1 + f2));
+//    }
+
+    private static void doubleAddition(int a, int b) {
+        System.out.println("Sum of two integers is " + (a + b));
+    }
+
+    private static void booleanType() {
+        System.out.println("Boolean method");
+    }
+}
