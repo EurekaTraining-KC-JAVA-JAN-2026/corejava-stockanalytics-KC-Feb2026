a999e5924b933e09db3bed6069e1b718e8937c65
AkshayReddy <Akshayreddybablu333@gmail.com>
2026-02-07 20:09:19 -0600

    Learnings from my 1st Class

---
+++ Eureka.java
@@ -0,0 +1,19 @@
+public class Eureka {
+    public static void main(String[] args) {
+        System.out.println("Welcome to Eureka...");
+        int i; //declare
+        i=20;//assign
+        int j =20;
+        System.out.println("value of i" +  i);
+
+
+
+
+        //double//
+        //boolean//
+        //long//
+        //use camelCasefor methods
+        //methodOverloading-Same method name but differentParameters
+
+    }
+}
