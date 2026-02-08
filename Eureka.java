4a409d44574c6b1bbb4acad1da475d4afbc2080e
parvathibitra <parvathibitra1996@gmail.com>
2026-02-07 20:07:56 -0600

    java first class about data types, method overloading, short cuts,and debug, evaluate etc..

---
+++ Eureka.java
@@ -0,0 +1,62 @@
+public class Eureka {
+    static void main() {
+        System.out.println("welcome to Eureka..!");
+
+        // ========== datatypes =========
+
+        //------ int
+
+        int i = 3;
+        int k;
+        k = 10;
+        System.out.println(" value of i " + i);
+        System.out.println(k);
+
+        //----- float
+        float f1 = 33.43f;
+        float f2 = 44.2f;
+        System.out.println(f1);
+        System.out.println("sum of f1 and f2 is " + (f1 + f2));
+
+
+        // -------- double
+
+        double d1 = 1.4;
+        double d2 = 40.3;
+        System.out.println("mul of d1 and d2 is " + (d1 * d2));
+
+        //----- boolean
+        boolean b1 = true;
+        b1 = false;
+        System.out.println(b1);
+
+
+        //------- long
+        longtype();
+
+
+
+        // non primitive
+
+        String s1 = "hare";
+        String s2 = "krishna";
+        System.out.println(s1 + " " + s2);
+
+
+
+
+    }
+
+
+
+    private static void longtype(long l1, long l2) {
+        long l1 = 134778738;
+        long l2 = 785271;
+        System.out.println(l1 + " and " + l2);
+    private static void longtype() {
+        long l1 = 134778738;
+        long l2 = 785271;
+        System.out.println(l1 + " and " + l2);
+    }
+
+}
