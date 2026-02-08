6749a0976cfd02ddded60bf17058aa299034a168
Nikhitha Mysolla <nikhithamysolla@gmail.com>
2026-02-08 11:47:44 -0600

    Add Methods.java

---
+++ Methods.java
@@ -0,0 +1,21 @@
+public class Methods {
+    public static void main(String[] args){
+        String name="Nikhitha";
+        String course="java";
+        float score=90.5f;
+        System.out.println("concatenation is :"+ (name+" "+course+" "+score));
+        concatenation();
+        String result=concatenation(name,course,score);
+        System.out.println("concatenation is :" +result);
+
+    }
+    private static String concatenation(String name,String course,float score) {
+        return name+" "+course+" "+score;
+    }
+    private static void concatenation(){
+        String name="Nikhitha";
+        String course="java";
+        float score=90.5f;
+        System.out.println("concatenation is :"+ (name+" "+course+" "+score));
+    }
+}
