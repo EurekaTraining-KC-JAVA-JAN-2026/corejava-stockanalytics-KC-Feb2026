f83529b673e380ddaaa0754942fd883a357c89c5
Harshitha <harshithamalladi20@gmail.com>
2026-02-08 14:41:50 -0600

    did my first java assignment

---
+++ Assignment.java
@@ -0,0 +1,35 @@
+public class Assignment {
+
+    public static void main(String[] args) {
+
+        String str = "hi";
+        String str2 = "hello";
+        float f = 2.22f;
+
+        String r1 = concat();
+        String r2 = concat(str, str2, f);
+        String r3 = concatOfTwo(str, str2, f);
+
+        System.out.println("First return is : " + r1);
+        System.out.println("Second return is: " + r2);
+        System.out.println("Third return  is : " + r3);
+    }
+
+    // 1️⃣ First way
+    private static String concat() {
+        String str = "hi";
+        String str2 = "hello";
+        float f = 2.22f;
+        return str + str2 + f;
+    }
+
+    // 2️⃣ Second way
+    private static String concat(String str, String str2, float f) {
+        return str + str2 + f;
+    }
+
+    // 3️⃣ Third way
+    public static String concatOfTwo(String str1, String str2, float f) {
+        return str1 + str2 + f;
+    }
+}
