aa1242f6de9bed8332c1fb74c502820a3fbc0d14
keerthi paruchuri <kumareureka89@gmail.com>
2026-02-08 19:31:55 -0600

    Done with classes, object's, constructers, pass-by-value, pass-by-ref

---
+++ Stock.java
@@ -0,0 +1,79 @@
+public class Stock {
+    //access modifiers
+    private String tickerSymbol;
+    private long marketCap;
+    private double currentRatio;
+    private int sectorId;
+    private int subSectorID;
+    //constr. with no param's
+    public Stock() {
+    }
+//constr. with 3 param's
+    public Stock(String tickerSymbol, long marketCap, double currentRatio) {
+        this.tickerSymbol = tickerSymbol;
+        this.marketCap = marketCap;
+        this.currentRatio = currentRatio;
+    }
+//constr. with 5 param's
+    public Stock(String tickerSymbol, long marketCap, double currentRatio, int sectorId, int subSectorID) {
+        this.tickerSymbol = tickerSymbol;
+        this.marketCap = marketCap;
+        this.currentRatio = currentRatio;
+        this.sectorId = sectorId;
+        this.subSectorID = subSectorID;
+    }
+
+    public double getCurrentRatio() {
+        return currentRatio;
+    }
+
+    public void setCurrentRatio(double currentRatio) {
+        this.currentRatio = currentRatio;
+    }
+
+    public long getMarketCap() {
+        return marketCap;
+    }
+
+    public void setMarketCap(long marketCap) {
+        this.marketCap = marketCap;
+    }
+
+    public int getSubSectorID() {
+        return subSectorID;
+    }
+
+    public void setSubSectorID(int subSectorID) {
+        this.subSectorID = subSectorID;
+    }
+
+
+    public String getTickerSymbol() {
+        return tickerSymbol;
+    }
+
+    public int getSectorId() {
+        return sectorId;
+    }
+
+    public void setSectorId(int sectorId) {
+        if(sectorId>=0){
+            this.sectorId = sectorId;
+        }
+    }
+
+    public void setTickerSymbol(String tickerSymbol) {
+        this.tickerSymbol = tickerSymbol;
+    }
+
+    @Override
+    public String toString() {
+        return "Stock{" +
+                "tickerSymbol='" + tickerSymbol + '\'' +
+                ", marketCap=" + marketCap +
+                ", currentRatio=" + currentRatio +
+                ", sectorId=" + sectorId +
+                ", subSectorID=" + subSectorID +
+                '}';
+    }
+}
