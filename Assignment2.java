88892b8d415ddaabf41c853993c5a48988d2489e
venkatasai3746 <venkatasais3746@gmail.com>
2026-02-10 14:34:35 -0600

    Added Assignment2

---
+++ Assignment2.java
@@ -0,0 +1,32 @@
+public class Assignment2 {
+    public static void main(String[] args) {
+        int x = 25;
+        int y = 45;
+        System.out.println("Pre-increment");
+        System.out.println("Value of x " + x);
+        System.out.println("Value of x with ++x " + (++x));
+        System.out.println("Value of x final " + x);
+
+        System.out.println("Post-increment");
+        System.out.println("Value of y " + y);
+        System.out.println("Value of y with y++ " + (y++));
+        System.out.println("Value of y final " + y);
+
+
+        int z = 50;
+        System.out.println("Pre-decrement");
+        System.out.println("Value of z " + z);
+        System.out.println("Value of z with --z " + (--z));
+        System.out.println("Value of z final " + z);
+
+        int a = 20;
+        System.out.println("Post-decrement");
+        System.out.println("Value of a " + a);
+        System.out.println("Value of a with a-- " + (a--));
+        System.out.println("Value of a final " + a);
+
+
+
+
+    }
+}
