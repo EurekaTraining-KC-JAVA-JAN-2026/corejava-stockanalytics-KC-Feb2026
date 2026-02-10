d6a7bc6e8031f4f6d77a8dc1b4cd4ef0b9401ab8
rohitharun15 <rohitharuns2k@gmail.com>
2026-02-10 16:13:34 -0600

    Assignment 2

---
+++ Assignment02092026.java
@@ -0,0 +1,25 @@
+public class Assignment02092026 {
+    public static void main(String[] args) {
+        int[] numArray= {1,2,3,4,5};
+
+        //post increment
+        for (int y=0;y<=numArray.length-1;y++){
+            System.out.println(numArray[y]);
+        }
+
+        //pre increment
+        for (int y=0;y<=numArray.length-1;++y){
+            System.out.println(numArray[y]);
+        }
+
+        //post decrement
+        for (int y=numArray.length-1;y>=0;y--){
+            System.out.println(numArray[y]);
+        }
+
+        //pre decrement
+        for (int y=numArray.length-1;y>=0;--y){
+            System.out.println(numArray[y]);
+        }
+    }
+}
