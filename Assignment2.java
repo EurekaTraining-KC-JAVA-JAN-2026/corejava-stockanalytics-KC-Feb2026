725ed0ad15d42b33712e32c2dceaaf16a855f336
Rajan3263 <soudarajanbhargav6299@gmail.com>
2026-02-10 16:37:14 -0600

    Assignment2

---
+++ Assignment2.java
@@ -0,0 +1,36 @@
+public class Assignment2 {
+    public static void main (String[] args){
+        int i = 5;
+        int j = ++i;
+        //pre increment
+        System.out.println("Pre increment");
+        System.out.println("The value of i is " +i); //pre increment
+        System.out.println("The value of j is " +j); //the value of i will be assigned to j
+       //post increment
+        int a = 5;
+        int b= a++;
+        System.out.println("post increment ");
+        System.out.println("The value of a is " +a);
+        System.out.println("the value of b is " +b);
+        //pre decrement
+        int c = 5;
+        int d = --c;
+        System.out.println("pre decrement");
+        System.out.println("The value of c is " +c);
+        System.out.println("The value of d is " +d);
+
+        //post decrement
+        int e = 5;
+        int f = e--;
+        System.out.println("post decrement");
+        System.out.println("The value of e is " +e);
+        System.out.println("The value of is " +f);
+
+
+        //by using loops
+        //int[] arr ={10,20,30,40,50};
+        //for(int y=0;y<arr.length;y++){
+           // System.out.println(arr[y]);
+        }
+    }
+
