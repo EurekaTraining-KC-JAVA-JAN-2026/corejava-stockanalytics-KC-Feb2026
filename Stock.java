eb0124328e8066b107cad9f19197e48cf34d9760
lohith05102001 <lohithnadapana@gmail.com>
2026-02-10 14:00:09 -0600

    Constructors by Getters and Setters and build the different stocks and used in diff files (Eureak.Java)

---
+++ Stock.java
@@ -0,0 +1,86 @@
+public class Stock {
+    //access modifier
+    //class having class Stock()
+    //constructor having only Stock()
+
+    private String tickerSymbol;
+    private long marketCap;
+    private double currentRatio;
+    private int sectorId;
+    private int subSectorId;
+    //constr. with no parameters
+    public Stock() {
+    }
+    //constructor with five parameters
+    public Stock(String tickerSymbol, long marketCap, double currentRatio, int sectorId, int subSectorId) {
+        this.tickerSymbol = tickerSymbol;
+        this.marketCap = marketCap;
+        this.currentRatio = currentRatio;
+        this.sectorId = sectorId;
+        this.subSectorId = subSectorId;
+    }
+
+
+
+
+
+    public long getMarketCap() {
+        return marketCap;
+    }
+
+    public void setMarketCap(long marketCap) {
+        this.marketCap = marketCap;
+    }
+
+
+
+
+    private double getCurrentRatio() {
+        return currentRatio;
+    }
+
+    public void setCurrentRatio(double currentRatio) {
+        this.currentRatio = currentRatio;
+    }
+
+    public int getSubSectorId() {
+        return subSectorId;
+    }
+
+    public void setSubSectorId(int subSectorId) {
+        this.subSectorId = subSectorId;
+    }
+
+
+
+    public int getSectorId() {
+        if(sectorId>=0)
+            this.sectorId=sectorId;
+        return sectorId;
+    }
+
+    public void setSectorId(int sectorId) {
+        this.sectorId = sectorId;
+    }
+
+
+
+    public String getTickerSymbol() {
+        return tickerSymbol;
+    }
+
+    public void setTickerSymbol(String tickerSymbol) {
+        this.tickerSymbol = tickerSymbol;
+    }
+
+    @Override
+    public String toString() {
+        return "Stock{" +
+                "tickerSymbol='" + tickerSymbol + '\'' +
+                ", marketCap=" + marketCap +
+                ", currentRatio=" + currentRatio +
+                ", sectorId=" + sectorId +
+                ", subSectorId=" + subSectorId +
+                '}';
+    }
+}
