04299cbc11d5cb040862ed9040a13631606b38d9
lohith05102001 <lohithnadapana@gmail.com>
2026-02-10 13:59:10 -0600

    Add StringPlayGround demonstrating string immutability, methods, static block, and string splitting

---
+++ StaticPlayGround.java
@@ -0,0 +1,12 @@
+public class StaticPlayGround {
+    public static int tyres = 4;
+    public static void move() {
+        System.out.println("Moving Forward");
+    }
+    public static void main(String[] args) {
+        System.out.println("Explaining Static");
+        move();
+//        System.out.println(tyres);
+        //public void Honda()
+    }
+}
