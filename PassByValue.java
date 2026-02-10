d7f1421dbeed1ac882cca9ea54a8b236c9e5cf30
lohith05102001 <lohithnadapana@gmail.com>
2026-02-10 14:23:58 -0600

    Here java will pass the value and pass by reference and learned about the identity hash code

--- PassByValue.java
+++ PassByValue.java
@@ -9,8 +9,6 @@ public class PassByValue {
          * it will only pass copied value of it
          * 2065951873     20
          */
-
-
         //Here java will pass the value, :: copy 20 pass it update()
         System.out.println("Before getting the update");
         update(x);
