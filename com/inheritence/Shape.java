67f33317e522a5b905d3aee6c71c527fce49a4e3
Laksheeth <laksheethinavolu>
2026-02-10 20:47:03 -0600

    topics covered abstraction ,inheritance,polymorphism,interfaces

---
+++ com/inheritence/Shape.java
@@ -0,0 +1,17 @@
+package com.inheritence;
+
+import java.math.BigDecimal;
+
+//here we will don some operations on shapes
+//show the contracts
+//circle,rectangle,square
+//interface are only to hold abstract type of methods, for our benefit it will have a default method(type regular)
+public interface Shape {
+    BigDecimal calculateArea();
+    BigDecimal calculatePerimeter();
+    BigDecimal calculateCircumferenec();
+
+    default void geometry(){
+        System.out.println("Printing Geometry");
+    }
+}
