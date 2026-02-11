0ac159a3f0ded0350cdb3f273281d5e462b0402a
ramya <ramyasreebusam99@gmail.com>
2026-02-10 23:23:33 -0600

    Abstract classes and methods, inheritance, polymorphism and interfaces

---
+++ com/eurekaAccounts/Accounts.java
@@ -0,0 +1,53 @@
+package com.eurekaAccounts;
+
+import java.math.BigDecimal;
+
+public abstract class Accounts {
+    /**
+     * Bank accounts
+     * checkings &savings to understand inheritance in java
+     */
+    public String accNumber;
+    public BigDecimal accBalance;
+    //one is parameterized
+    public Accounts( String accNumber,BigDecimal accBalance) {
+        this.accBalance = accBalance;
+        this.accNumber = accNumber;
+    }
+    //one is default
+    public Accounts() {
+    }
+
+    public BigDecimal getAccBalance() {
+        return accBalance;
+    }
+
+    public void setAccBalance(BigDecimal accBalance) {
+        this.accBalance = accBalance;
+    }
+
+    public String getAccNumber() {
+        return accNumber;
+    }
+
+    public void setAccNumber(String accNumber) {
+        this.accNumber = accNumber;
+    }
+    //method to depositAmount
+    public BigDecimal depositAmount(BigDecimal deposit){
+        setAccBalance(accBalance.add(deposit));
+        return getAccBalance();
+    }
+    //method to withdrawAmount
+    public BigDecimal withdrawAmount(BigDecimal withdraw){
+        setAccBalance(accBalance.subtract(withdraw));
+        return getAccBalance();
+    }
+    public void something(){
+
+    };
+    //method to see accountDetails
+    //wont create any memory just registers with method signature
+    public abstract void printAccountDetails();
+
+}
---
+++ com/inheritance/Rectangle.java
@@ -0,0 +1,28 @@
+package com.inheritance;
+
+import java.math.BigDecimal;
+
+public class Rectangle implements Shape {
+    public BigDecimal length;
+    public BigDecimal breadth;
+
+    public Rectangle(BigDecimal breadth, BigDecimal length) {
+        this.breadth = breadth;
+        this.length = length;
+    }
+
+    @Override
+    public BigDecimal calculateArea() {
+        return length.multiply(breadth);
+    }
+
+    @Override
+    public BigDecimal calculatePerimeter() {
+        return length.add(breadth).multiply(new BigDecimal(2));
+    }
+
+    @Override
+    public BigDecimal calculateCircumferenec() {
+        return null;
+    }
+}
