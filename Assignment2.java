0a9f63c8cffe957d01d12e6d790764a9e474f318
Harshitha <harshithamalladi20@gmail.com>
2026-02-10 22:45:07 -0600

    oops, abstract classes and interfaces

--- Assignment2.java
+++ Assignment2.java
@@ -7,12 +7,12 @@ public class Assignment2 {
         int a = 10;
         int b = a++;
         System.out.println(a); // Result 11 (Value increased after assignment)
-        System.out.println(b); // Result 10 (Assigned the original value of 'a')
+        System.out.println(b); // Result 10 (Assigned the original value of a )
 
         // Loop demonstrates i increases after each iteration finishes
-        for(int i = 0; i < 5; i++) {
-            System.out.println(i);
-        }
+        //for(int i = 0; i < 5; i++) {
+            //System.out.println(i);
+       // }
 
         // PRE INCREMENT
         //  The value is incremented by 1 FIRST then assigned
@@ -20,11 +20,11 @@ public class Assignment2 {
         int pre = 20;
         int increment = ++pre;
         System.out.println(pre);       // Result 21
-        System.out.println(increment); // Result 21 (Both are 21 because 'pre' was updated before assignment)
+        System.out.println(increment); // Result 21 (Both are 21 because pre was updated before assignment)
 
-        for(int j = 0; j < 6; ++j) {
-            System.out.println(j);
-        }
+        //for(int j = 0; j < 6; ++j) {
+          //  System.out.println(j);
+        //}
 
         // POST DECREMENT
         //  The value is used FIRST  then decreased by 1.
@@ -32,11 +32,11 @@ public class Assignment2 {
         int dec = 30;
         int value = dec--;
         System.out.println(dec);   // Result is 29
-        System.out.println(value); // Result is  30 (Assigned before 'dec' dropped to 29)
+        System.out.println(value); // Result is  30 (Assigned before dec dropped to 29)
 
-        for(int d = 3; d > 0; d--) {
-            System.out.println(d);
-        }
+        //for(int d = 3; d > 0; d--) {
+            //System.out.println(d);
+        //}
 
         //  PRE DECREMENT
         //  The value is decreased by 1 FIRST then used.
@@ -46,11 +46,12 @@ public class Assignment2 {
         System.out.println(predec);        // Result 39
         System.out.println(decrementValue); // Result 39 (Both reflect the new value)
 
-        for(int pr = 3; pr > 0; pr--) {
-            System.out.println(pr);
+        //for(int pr = 3; pr > 0; pr--) {
+            //System.out.println(pr);
         }
     }
-}
+
+
 
 
 
---
+++ com/eurekaAccounts/Accounts.java
@@ -0,0 +1,56 @@
+package com.eurekaAccounts;
+
+import java.math.BigDecimal;
+
+public  abstract class Accounts {
+    // ban accounts checking and saving accounts
+    public String accNumber;
+    public BigDecimal accBalance;
+
+    public String getAccNumber() {
+        return accNumber;
+    }
+
+    public void setAccNumber(String accNumber) {
+        this.accNumber = accNumber;
+    }
+
+    public BigDecimal getAccBalance() {
+        return accBalance;
+    }
+
+    public void setAccBalance(BigDecimal accBalance) {
+        this.accBalance = accBalance;
+    }
+    public BigDecimal depositAmount(BigDecimal deposit)
+    {
+        setAccBalance(accBalance.add(deposit));
+        return accBalance;
+    }
+    public BigDecimal withdrawAmount(BigDecimal withdraw)
+    {
+        setAccBalance(accBalance.subtract(withdraw));
+        return accBalance;
+    }
+
+    public void something(){
+
+    }
+
+    public Accounts(String accNumber, BigDecimal accBalance) {
+        this.accNumber = accNumber;
+        this.accBalance = accBalance;
+    }
+
+    public Accounts() {
+    }
+    public abstract void printAccountDetails();
+
+    @Override
+    public String toString() {
+        return "Accounts{" +
+                "accNumber='" + accNumber + '\'' +
+                ", accBalance=" + accBalance +
+                '}';
+    }
+}
