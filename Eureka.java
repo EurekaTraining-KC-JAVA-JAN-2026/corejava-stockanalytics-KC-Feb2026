3c537705695cdccf5fafcd7030c2ad6d2b675b8b
parisanetra <netraniveditha07@gmail.com>
2026-02-16 17:16:07 -0600

    i have did with control statments and override methods

--- Eureka.java
+++ Eureka.java
@@ -1,71 +1,71 @@
 public class Eureka {
     public static void main(String[] args) {
-        System.out.println("Welcome Eureka...!");
-        System.out.println("calling static from here...");
-        String s1= "eureka";
-        s1.contains("e");
-        int i;
-        i = 10;
-        int j = 20;
 
+        System.out.println("Calling static methods..");
+        StaticPlayGround staticPlayGround = new StaticPlayGround();
 
-       // Stock googleStock = new Stock("GOOG", 7656776, 13.4, 32, 144);
-        //System.out.println(googleStock.getTickerSymbol()+" "+googleStock+ googleStock.getSectorId());
+        //Calling through class directly
+        StaticPlayGround.move();
+        //System.out.println("Welcome Eureka!");
+        //primitives
+        //1. int
+        intAddition();
+        //2.float
+        float f1 = 23.45f;
+        float f2 = 12.15f;
+        //System.out.println("sum of 2 floats is : " + (f1 + f2));
 
-        Stock appleStock = new Stock();
-        appleStock.setTickerSymbol("APPL");//setting the ticker symbol
-        appleStock.setSectorId(23);
-        appleStock.setMarketCap(222333);
-        appleStock.setSubSectorId(123);
-        appleStock.setCurrentRatio(23.55);
-        //accessing the application in appleStock tickerSymbol
-       // System.out.println(appleStock.getTickerSymbol()+" "+appleStock+ appleStock.getSectorId());
+        //3.double
+        double d1 = 12.36;
+        double d2 = 9.80;
+        addingDoubleNumbers(d1, d2);
 
-
-
-
-
-        //String s1 = new String(original:"lohith");
-
-
-        /**
-         * This will return the double
-         * @param d1
-         * @param d2
-         * @retuen
-         */
-
-
-
-       // System.out.println("value of i" + i);
-       // System.out.println("value of j" + j);
-        //float
-        floatAddition();
-        //double
-        double d1 = 12.34;
-        double d2 = 34.12;
-        //System.out.println("multi of 2 doubles is " + (d1 * d2));
-        //boolean
+        //4. boolean
         boolean b1 = true;
-        b1 = false;
-       // System.out.println("value of boolean is " + b1);
-        //long
-        long l1 = 123456;
-        long l2 = 654321;
-      //  System.out.println("value of 2 longs is " + l1 + " and " + l2);
-        //non-primitive
-        //String s1 = "Eureka";
-        String s2 = new String("tech");
-        System.out.println("concatenation of s1 and s2 is " + s1 + " " + s2);
+        boolean b2 = false;
+        //System.out.println("types of boolean :" + b1);
+        //System.out.println("types of boolean :" + b2);
 
+        //5. Long
+        long l1 = 22L;
+        long l2 = 23L;
+        //System.out.println(l1 + l2);
+
+        //6. Char
+        char c1 = 'V';
+        char c2 = 'N';
+        // System.out.println(c1 + c2);
+
+        //non-primitives
+
+        //1. String
+        String name = "Eureka";
+        String s1 = new String("ABC");
+        System.out.println(name + " " + s1);
+
+        Stock stock = new Stock();
+        stock.setTickerSymbol("AAPL");
+        stock.setSectorId(12);
+        stock.setSubsectorId(200);
+
+        System.out.println(stock.getTickerSymbol() + " " + stock.getSectorId());
+
+        Stock newStock = new Stock("AAPL", 23, 345672, "22.3", 12);
+        System.out.println(newStock);
 
     }
 
-    private static void floatAddition() {
-        float f1 = 34.22f;
-        float f2 = 22.34f;
-        System.out.println("sum of 2 floats is " + (f1 + f2));
+    private static double addingDoubleNumbers(double d1, double d2) {
+        //System.out.println("multi of d1 and d2 is : " + (d1 * d2));
+        return d1 + d2;
+    }
 
+    private static void intAddition() {
+        int i;
+        i = 2;
+        int j = 23;
+        //System.out.println("value of I is : " + i);
+        //System.out.println("Value of J is : " + j);
     }
 
 }
--- Stock.java
+++ Stock.java
@@ -1,62 +1,31 @@
-public class Stock {
-    //access modifier
+import java.util.Objects;
 
+public class Stock {
+
+    /**
+     * properties
+     */
     private String tickerSymbol;
-    private long marketCap;
-    private double currentRatio;
     private int sectorId;
-    private int subSectorId;
+    private int marketCap;
+    private String currentRatio;
+    private int subsectorId;
+
+    public Stock(String tickerSymbol, int sectorId) {
+        this.tickerSymbol = tickerSymbol;
+        this.sectorId = sectorId;
+    }
 
     public Stock() {
     }
 
-    public Stock(String tickerSymbol, long marketCap, double currentRatio, int sectorId, int subSectorId) {
+    public Stock(String tickerSymbol, int sectorId, int marketCap, String currentRatio, int subsectorId) {
         this.tickerSymbol = tickerSymbol;
+        this.sectorId = sectorId;
         this.marketCap = marketCap;
         this.currentRatio = currentRatio;
-        this.sectorId = sectorId;
-        this.subSectorId = subSectorId;
+        this.subsectorId = subsectorId;
     }
-
-    public long getMarketCap() {
-        return marketCap;
-    }
-
-    public void setMarketCap(long marketCap) {
-        this.marketCap = marketCap;
-    }
-
-
-
-    private double getCurrentRatio() {
-        return currentRatio;
-    }
-
-    public void setCurrentRatio(double currentRatio) {
-        this.currentRatio = currentRatio;
-    }
-
-    public int getSubSectorId() {
-        return subSectorId;
-    }
-
-    public void setSubSectorId(int subSectorId) {
-        this.subSectorId = subSectorId;
-    }
-
-
-
-    public int getSectorId() {
-        if(sectorId>=0)
-            this.sectorId=sectorId;
-        return sectorId;
-    }
-
-    public void setSectorId(int sectorId) {
-        this.sectorId = sectorId;
-    }
-
-
 
     public String getTickerSymbol() {
         return tickerSymbol;
@@ -64,5 +33,78 @@ public class Stock {
 
     public void setTickerSymbol(String tickerSymbol) {
         this.tickerSymbol = tickerSymbol;
+    }
+
+    public int getSectorId() {
+        return sectorId;
+    }
+
+    public int getMarketCap() {
+        return marketCap;
+    }
+
+    public void setMarketCap(int marketCap) {
+        this.marketCap = marketCap;
+    }
+
+    public String getCurrentRatio() {
+        return currentRatio;
+    }
+
+    public void setCurrentRatio(String currentRatio) {
+        this.currentRatio = currentRatio;
+    }
+
+    public int getSubsectorId() {
+        return subsectorId;
+    }
+
+    public void setSubsectorId(int subsectorId) {
+        this.subsectorId = subsectorId;
+    }
+
+    public void setSectorId(int sectorId) {
+
+        if (sectorId > 0) {
+            this.sectorId = sectorId;
+        } else {
+            throw (new RuntimeException("SectorID shoudl be greater than 0"));
+        }
+    }
+//
+//    @Override
+//    public boolean equals(Object o) {
+//        if (o == null || getClass() != o.getClass()) return false;
+//        Stock stock = (Stock) o;
+//        return sectorId == stock.sectorId && Objects.equals(tickerSymbol, stock.tickerSymbol);
+//    }
+
+    public boolean equals(Object outsideObj) {
+        boolean isEquals = false;
+        if (outsideObj != null && (outsideObj instanceof Stock)) {
+            Stock outSide = (Stock) outsideObj;
+            if (this.getTickerSymbol().equals(outSide.getTickerSymbol()) && this.sectorId == outSide.getSectorId()) {
+                isEquals=true;
+            } else  {
+                isEquals=false;
+            }
+        }
+        return isEquals;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(tickerSymbol, sectorId);
+    }
+
+    @Override
+    public String toString() {
+        return "Stock{" +
+                "tickerSymbol='" + tickerSymbol + '\'' +
+                ", sectorId=" + sectorId +
+                ", marketCap=" + marketCap +
+                ", currentRatio='" + currentRatio + '\'' +
+                ", subsectorId=" + subsectorId +
+                '}';
     }
 }
