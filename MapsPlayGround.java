a7b80a00b2dcaa03ed48f15415682e99c6a921a0
lohith05102001 <lohithnadapana@gmail.com>
2026-02-18 14:07:31 -0600

    Assignment on Map Functions

--- MapsPlayGround.java
+++ MapsPlayGround.java
@@ -17,6 +17,27 @@ public class MapsPlayGround {
 
         boolean map= mapStock.equals("AMD");
         System.out.println(map);
+        mapStock.get("AMD");
+        System.out.println(mapStock.get("AMD"));
+        mapStock.put("JMP", "JP Morgan Chase");
+        System.out.println(mapStock);
+        mapStock.size();
+        System.out.println(mapStock.size());
+        mapStock.remove("AMD");
+        System.out.println(mapStock);
+        Map<String,String> mapStock1= new HashMap<>();
+        mapStock.put("TCS","Tata consulating Service");
+        mapStock.put("RACE", "Ferrari N.V");
+        mapStock.putAll(mapStock1);
+        System.out.println(mapStock);
+        mapStock1.clear();
+        System.out.println(mapStock1);
+        System.out.println(mapStock);
+        boolean maps= mapStock.isEmpty();
+        System.out.println(maps);
+        mapStock.computeIfAbsent("PPL", key-> "PEOPLE");
+        System.out.println(mapStock.get("PPL"));
+
 
 
     }
