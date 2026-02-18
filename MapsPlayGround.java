d47e8106db446bf17627a3b9359026710c3d050a
sathvikaporeddy24 <sathvikaporeddy24@gmail.com>
2026-02-18 16:31:03 -0600

    Explored different map methods

--- MapsPlayGround.java
+++ MapsPlayGround.java
@@ -1,3 +1,5 @@
+import org.w3c.dom.ls.LSOutput;
+
 import java.util.HashMap;
 import java.util.Map;
 
@@ -9,7 +11,37 @@ public class MapsPlayGround {
         mapStock.put("AAPL", "Apple INC");
         mapStock.put("MSFT","Microsoft");
         mapStock.put("TSLA","Tsla INC");
+        mapStock.put("GOOG","Google");
         System.out.println(mapStock);
         System.out.println(mapStock.get("AMD"));
+        //Assignment-explore diff methods in maps, similar to lists
+        mapStock.get("MSFT"); //returns the value Microsoft
+        mapStock.get("NVDA"); //returns null
+        mapStock.containsKey("MSFT");
+        mapStock.isEmpty()
+        //for non-existing keys it returns null value, so we need to add to a newList
+        if(mapStock.containsKey("TSLA")){
+            System.out.println("Tesla exits"); //checks if key exists
+        }
+        boolean found = mapStock.containsValue("Google");
+        System.out.println(found);
+
+        mapStock.replace("GOOG", "Google INC");
+
+        mapStock.remove("AMD");
+        System.out.println("The size of current map is :" + mapStock.size());
+        for( String key : mapStock.keySet()){ //using for each to returns only all keys from the map
+            System.out.println(key);
+        }
+
+        for ( String value : mapStock.values()){ //returns all values iterating each element one by one
+            System.out.println(value);
+        }
+
+        for (Map.Entry<String, String> entry : mapStock.entrySet()){ //returns both key and value pairs of all elements
+            System.out.println(entry.getKey() + " : " + entry.getValue());
+        }
+
+
     }
 }
