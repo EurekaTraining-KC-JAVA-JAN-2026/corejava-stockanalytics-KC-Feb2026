165427b6687ef3c1c6b973bc172609b29ade9b3d
harishchandraguntupalli98 <harishchandraguntupalli98@gmail.com>
2026-02-18 14:21:20 -0600

    Maps Assignment

--- MapsPlayGround.java
+++ MapsPlayGround.java
@@ -1,5 +1,8 @@
+import java.sql.SQLOutput;
+import java.util.Collection;
 import java.util.HashMap;
 import java.util.Map;
+import java.util.Set;
 
 public class MapsPlayGround {
     public static void main(String[] args) {
@@ -13,5 +16,37 @@ public class MapsPlayGround {
         System.out.println(mapStock.get("AAPL"));
         boolean m = mapStock.containsKey("AAPL");
         System.out.println(m);
+        boolean hasValue = mapStock.containsValue("Google");
+        System.out.println("Map has the value :"+hasValue);
+        System.out.println("Size of map: " + mapStock.size());
+        mapStock.remove("TSLA");
+        System.out.println("After removing Tesla :"+mapStock);
+        mapStock.replace("GOOG","Google LLC");
+        System.out.println("After replacing :"+mapStock);
+        Set<String> keys = mapStock.keySet();
+        System.out.println(keys);
+
+        Collection<String> values = mapStock.values();
+        System.out.println("Values: " + values);
+
+        System.out.println("EntrySet:");//best for iterating and gets both key and values
+        for(Map.Entry<String,String> entry : mapStock.entrySet()){
+            System.out.println(entry.getKey()+" : "+entry.getValue());
+        }
+
+        System.out.println("KeySet:");//should use it only when we need keys
+        for(String key : mapStock.keySet()){
+            System.out.println(key);
+        }
+        System.out.println("Values :");//should use it only when we need values
+        for (String value : mapStock.values()){
+            System.out.println(value);
+        }
+
+        System.out.println("Is map empty: " + mapStock.isEmpty());
+        mapStock.clear();
+        System.out.println("After clear: " + mapStock);
+
+
     }
 }
