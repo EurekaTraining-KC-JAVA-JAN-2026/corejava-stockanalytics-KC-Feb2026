d86653f2dd94f959206e0c8a72e65339c0f04727
Rajasimha9912-dev <rajasimhareddy05@gmail.com>
2026-02-18 13:03:41 -0600

    explored map mathods

--- MapsPlayGround.java
+++ MapsPlayGround.java
@@ -11,5 +11,15 @@ public class MapsPlayGround {
         System.out.println(mapstock);
         System.out.println(mapstock.get("AMD"));
         // assignment explore methods
+
+        System.out.println(mapstock.containsKey("AAPL"));
+        System.out.println(mapstock.keySet());
+        System.out.println(mapstock.size());
+        System.out.println(mapstock.containsValue("Tesla stock"));
+        System.out.println(mapstock.isEmpty());
+        mapstock.put("ASD","new stock");
+        
+        //when we add the key->value pair through put it is automatically arranges it in the assending order.
+        System.out.println(mapstock);
     }
 }
